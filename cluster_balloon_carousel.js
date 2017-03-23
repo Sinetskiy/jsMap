73997cd940aeb3f788faff5553fb72e2f7515aaa
sinetskiy_as@open.ru <andrey.sinetskiy@open.ru>
2017-03-23 19:29:06 +0300

    second commit

--- cluster_balloon_carousel.js
+++ cluster_balloon_carousel.js
@@ -5,7 +5,10 @@ ymaps.ready(function() {
             zoom: 9,
             controls: []
         }),
-        coords;
+        coords,
+        placemarks = [],
+        points = [],
+        pointId = 0;
 
     // Создаем собственный макет с информацией о выбранном геообъекте.
     var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
@@ -30,13 +33,7 @@ ymaps.ready(function() {
         clusterBalloonContentLayoutHeight: 130,
         // Устанавливаем максимальное количество элементов в нижней панели на одной странице
         clusterBalloonPagerSize: 5
-            // Настройка внешего вида нижней панели.
-            // Режим marker рекомендуется использовать с небольшим количеством элементов.
-            // clusterBalloonPagerType: 'marker',
-            // Можно отключить зацикливание списка при навигации при помощи боковых стрелок.
-            // clusterBalloonCycling: false,
-            // Можно отключить отображение меню навигации.
-            // clusterBalloonPagerVisible: false
+
     });
 
     var link = document.querySelector('#map');
@@ -47,11 +44,12 @@ ymaps.ready(function() {
     map.events.add('click', function(e) {
         if (!map.balloon.isOpen()) {
             e.preventDefault();
+            pointId++;
             coords = e.get('coords');
             clientPixels = e.get('clientPixels');
-            popup.style.left = clientPixels[0] - popup.offsetWidth + 'px';
+            popup.style.left = clientPixels[0] + 'px';
             popup.style.top = clientPixels[1] + 'px';
-            ymaps.geocode(e.get('coords'), { kind: 'street' }).then(function(res) {
+            ymaps.geocode(coords, { kind: 'street' }).then(function(res) {
                 var nearestStreet = res.geoObjects.get(0);
                 headerText.innerText = `${nearestStreet.properties.get('description')}, ${nearestStreet.properties.get('name')}`;
                 popup.style.display = 'block';
@@ -61,33 +59,46 @@ ymaps.ready(function() {
         }
     });
 
+    link.addEventListener('click', function(e) {
+        if (!e.target.dataset.id) {
+            return;
+        }
+        debugger;
+
+    });
+
     add.addEventListener('click', function(e) {
-        var placemarks = [];
+        var point = {
+            userName: document.getElementById('feedback-name').value,
+            place: document.getElementById('feedback-place').value,
+            streatName: headerText.innerText,
+            feedback: document.getElementById('feedback-textarea').value,
+            date: new Date()
+        }
         var placemark = new ymaps.Placemark(coords, {
-            // Устаналиваем данные, которые будут отображаться в балуне.
-            //   balloonContentHeader: document.getElementById('feedback-name').value,
-            balloonContentHeader: document.getElementById('feedback-place').value,
-            balloonContentBody: `<p>${headerText.innerText}</p> 
-                                 <p>${document.getElementById('feedback-textarea').value}</p>`,
-            balloonContentFooter: (new Date()).toLocaleString("ru")
+            balloonContentHeader: point.place,
+            balloonContentBody: `<p><a href="#" data-id="${point.id}" >${point.streatName}</a></p> 
+                                 <p>${point.feedback}</p>`,
+            balloonContentFooter: point.date.toLocaleString("ru")
         });
         placemarks.push(placemark);
-
+        points.push(point);
         clusterer.add(placemarks);
-
         map.geoObjects.add(clusterer);
+        //placemarks[3].geometry.getCoordinates()
+        debugger;
+        sessionStorage.setItem(pointId, JSON.stringify(points));
     });
 
     document.getElementById('hider').onclick = function() {
         document.getElementById('popup').style.display = 'none';
     }
 
-    function getRandomPosition() {
-        return [
-            mapCenter[0] + (Math.random() * 0.3 - 0.15),
-            mapCenter[1] + (Math.random() * 0.5 - 0.25)
-        ];
+    function getLastId() {
+        return 0;
     }
+
+
 
     clusterer.balloon.open(clusterer.getClusters()[0]);
 });
